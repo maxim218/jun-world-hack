2f51d149598e597d2a4917fb7c2f295eea388ff8
Maxim Kolotovkin <maxim.kolotovkin@ibm.com>
2019-10-26 19:56:24 +0300

    New wall

--- static/code.js
+++ static/code.js
@@ -284,6 +284,7 @@ function addWall(x, y, w, h) {
 addWall(100, 400, 150, 160);
 addWall(500, 300, 150, 260);
 addWall(-4000, 550, 8000, 100);
+addWall(950, 400, 150, 160);
 
 function getWallsArray() {
     return arr;
